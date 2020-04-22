fe84e5cfd3e7903b4c9fe256cccd076679cf11ae
SEONGGYU96 <link5658@gmail.com>
2020-04-23 01:16:24 +0900

    feat : Add splash activity
    This activity does not have delay but start main activity immediately.

---
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.seoultech.livingtogether_android.ui
+
+import com.seoultech.livingtogether_android.R
+import com.seoultech.livingtogether_android.base.BaseActivity
+import com.seoultech.livingtogether_android.databinding.ActivityMainBinding
+
+
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+}
---
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.seoultech.livingtogether_android.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
