08e717ee7208505d4dce45616ab57f94a59ad1ba
SEONGGYU96 <link5658@gmail.com>
2020-05-16 00:42:32 +0900

    feat: Room DB 구조 구축

---
+++ app/src/main/java/com/seoultech/livingtogether_android/model/room/entity/UserEntity.kt
@@ -0,0 +1,21 @@
+package com.seoultech.livingtogether_android.model.room.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_entity")
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true) var userId: Int = 0,
+
+    @ColumnInfo(name = "name") var name: String = "",
+
+    @ColumnInfo(name = "address") var address: String = "",
+
+    @ColumnInfo(name = "phone_num") var phoneNum: String = "",
+
+    //TODO: 지역구 Enum 클래스 만들기
+    @ColumnInfo(name = "district") var district: String = "",
+
+    @ColumnInfo(name = "last_location") var lastLocation: String = ""
+)
