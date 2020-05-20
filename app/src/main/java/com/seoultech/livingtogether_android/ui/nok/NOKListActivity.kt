7900a04798b9e048738808fc92d219465e49911e
SEONGGYU96 <link5658@gmail.com>
2020-05-20 21:14:33 +0900

    feat: Add NOKListActivity

---
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/nok/NOKListActivity.kt
@@ -0,0 +1,16 @@
+package com.seoultech.livingtogether_android.ui.nok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seoultech.livingtogether_android.R
+import com.seoultech.livingtogether_android.base.BaseActivity
+import com.seoultech.livingtogether_android.databinding.ActivityNokListBinding
+
+class NOKListActivity : BaseActivity<ActivityNokListBinding>(R.layout.activity_nok_list) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+}
