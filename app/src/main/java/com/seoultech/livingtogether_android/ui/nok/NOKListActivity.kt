5ea78425edf9cd222fa3cc33b25f6a114cfdb808
SEONGGYU96 <link5658@gmail.com>
2020-05-25 14:50:44 +0900

    feat: Add a recycler view in NOKListActivity

--- app/src/main/java/com/seoultech/livingtogether_android/ui/nok/NOKListActivity.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/nok/NOKListActivity.kt
@@ -3,11 +3,16 @@ package com.seoultech.livingtogether_android.ui.nok
 import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.seoultech.livingtogether_android.R
+import com.seoultech.livingtogether_android.adapter.NOKAdapter
 import com.seoultech.livingtogether_android.base.BaseActivity
 import com.seoultech.livingtogether_android.databinding.ActivityNokListBinding
+import com.seoultech.livingtogether_android.ui.main.decoration.MarginDecoration
 
 class NOKListActivity : BaseActivity<ActivityNokListBinding>(R.layout.activity_nok_list) {
+    private val nokAdapter: NOKAdapter by lazy { NOKAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +21,10 @@ class NOKListActivity : BaseActivity<ActivityNokListBinding>(R.layout.activity_n
             buttonAddNokList.setOnClickListener {
                 //Todo: 입력하여 추가하기 vs 주소록에서 추가하기 다이얼로그 띄우기
                 startActivity(Intent(this@NOKListActivity, AddNOKActivity::class.java))
+
+                recyclerNokList.layoutManager = LinearLayoutManager(baseContext)
+                recyclerNokList.adapter = nokAdapter
+                recyclerNokList.addItemDecoration(MarginDecoration(baseContext, 15, RecyclerView.VERTICAL))
             }
         }
 
