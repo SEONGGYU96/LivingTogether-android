66c66a2df690890491fedc17a9e0182832a713ab
SEONGGYU96 <link5658@gmail.com>
2020-05-29 04:02:48 +0900

    feat: Add a back button in toolbar

--- app/src/main/java/com/seoultech/livingtogether_android/ui/profile/EditProfileActivity.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/profile/EditProfileActivity.kt
@@ -2,6 +2,7 @@ package com.seoultech.livingtogether_android.ui.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.seoultech.livingtogether_android.R
 import com.seoultech.livingtogether_android.base.BaseActivity
 import com.seoultech.livingtogether_android.databinding.ActivityEditProfileBinding
@@ -10,5 +11,22 @@ class EditProfileActivity : BaseActivity<ActivityEditProfileBinding>(R.layout.ac
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setSupportActionBar(binding.toolbarEditProfile)
+
+        supportActionBar?.let {
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_32dp)
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
