669dddce204da29c6b91917fad95e041b27c23c9
SEONGGYU96 <link5658@gmail.com>
2020-05-29 01:07:14 +0900

    feat: Add a EditProfileActivity

---
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/profile/EditProfileActivity.kt
@@ -0,0 +1,14 @@
+package com.seoultech.livingtogether_android.ui.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.seoultech.livingtogether_android.R
+import com.seoultech.livingtogether_android.base.BaseActivity
+import com.seoultech.livingtogether_android.databinding.ActivityEditProfileBinding
+
+class EditProfileActivity : BaseActivity<ActivityEditProfileBinding>(R.layout.activity_edit_profile) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
