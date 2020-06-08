8248f5bc8d2b6e75d65a93a1cb300921270d082d
SEONGGYU96 <link5658@gmail.com>
2020-06-09 01:08:29 +0900

    refector: Change data type of adapter
    Please implement SignalHistoryEntity.getDetectedTimeToString().

--- app/src/main/java/com/seoultech/livingtogether_android/debug/scan/adapter/ScanServiceTestAdapter.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/debug/scan/adapter/ScanServiceTestAdapter.kt
@@ -4,10 +4,10 @@ import android.view.ViewGroup
 import com.seoultech.livingtogether_android.base.BaseAdapter
 import com.seoultech.livingtogether_android.base.BaseViewHolder
 import com.seoultech.livingtogether_android.debug.scan.adapter.viewholder.BeaconSignalDetectedTimeViewHolder
-import com.seoultech.livingtogether_android.debug.model.SignalTestModel
+import com.seoultech.livingtogether_android.model.room.entity.SignalHistoryEntity
 
-class ScanServiceTestAdapter() : BaseAdapter<SignalTestModel>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<SignalTestModel, *> {
+class ScanServiceTestAdapter() : BaseAdapter<SignalHistoryEntity>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<SignalHistoryEntity, *> {
         return BeaconSignalDetectedTimeViewHolder(parent)
     }
 }
--- app/src/main/java/com/seoultech/livingtogether_android/debug/scan/adapter/viewholder/BeaconSignalDetectedTimeViewHolder.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/debug/scan/adapter/viewholder/BeaconSignalDetectedTimeViewHolder.kt
@@ -5,14 +5,15 @@ import com.seoultech.livingtogether_android.R
 import com.seoultech.livingtogether_android.base.BaseViewHolder
 import com.seoultech.livingtogether_android.databinding.ItemBeaconSignalDetectedTimeBinding
 import com.seoultech.livingtogether_android.model.room.entity.DeviceEntity
+import com.seoultech.livingtogether_android.model.room.entity.SignalHistoryEntity
 
-class BeaconSignalDetectedTimeViewHolder(parent: ViewGroup) : BaseViewHolder<DeviceEntity, ItemBeaconSignalDetectedTimeBinding>(
+class BeaconSignalDetectedTimeViewHolder(parent: ViewGroup) : BaseViewHolder<SignalHistoryEntity, ItemBeaconSignalDetectedTimeBinding>(
     R.layout.item_beacon_signal_detected_time, parent) {
 
-    override fun bind(data: DeviceEntity) {
+    override fun bind(data: SignalHistoryEntity) {
         binding.run {
             textMajorScanTestItem.text = data.deviceMajor
-            textDetectedTimeScanTestItem.text = data.getLastDetectedTypeOneToString()
+            textDetectedTimeScanTestItem.text = data.getDetectedTimeToString()
         }
     }
 }
