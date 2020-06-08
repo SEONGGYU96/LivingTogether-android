1759984ffcb0b7be3dfefbe6d6631a2fe0ef4833
SEONGGYU96 <link5658@gmail.com>
2020-06-09 01:11:36 +0900

    feat: Add method for getting detected time as string

--- app/src/main/java/com/seoultech/livingtogether_android/model/room/entity/SignalHistoryEntity.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/model/room/entity/SignalHistoryEntity.kt
@@ -3,6 +3,7 @@ package com.seoultech.livingtogether_android.model.room.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "signal_history_entity")
 data class SignalHistoryEntity(
@@ -11,4 +12,11 @@ data class SignalHistoryEntity(
     @ColumnInfo(name = "detective_time") var detectiveTime: Long
 ) {
     @PrimaryKey(autoGenerate = true) var id:Int = 0
+
+    private fun getDetectedTimeToString() : String {
+        val calendar = GregorianCalendar()
+        calendar.timeInMillis = detectiveTime
+        return "${calendar.get(Calendar.YEAR)}/${calendar.get(Calendar.MONTH)}/${calendar.get(
+            Calendar.DAY_OF_MONTH)} ${calendar.get(Calendar.HOUR_OF_DAY)} : ${calendar.get(Calendar.MINUTE)}"
+    }
 }
