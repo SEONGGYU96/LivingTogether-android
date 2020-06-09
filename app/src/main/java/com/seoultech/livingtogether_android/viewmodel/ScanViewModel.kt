24f8c50f4509b7fd90a462c9c2620c17bafc2514
SEONGGYU96 <link5658@gmail.com>
2020-06-10 04:51:20 +0900

    fix: Finish after trying detect same device again

--- app/src/main/java/com/seoultech/livingtogether_android/viewmodel/ScanViewModel.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/viewmodel/ScanViewModel.kt
@@ -122,6 +122,7 @@ class ScanViewModel(application: Application) : AndroidViewModel(application) {
                     if (db.deviceDao().getAll().isNotEmpty()) {
                         //Todo: 이미 등록된 버튼이라는 다이얼로그 띄우기
                         Log.d(TAG, "This device is already registered. return.")
+                        isFound.value = true
                     }
 
                     val calendar = GregorianCalendar()
