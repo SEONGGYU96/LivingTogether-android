8c414dcad139d68ea59ca4314fae90dd1e05e66d
SEONGGYU96 <link5658@gmail.com>
2020-06-21 04:09:15 +0900

    refactor: Use BaseDao

--- app/src/main/java/com/seoultech/livingtogether_android/model/room/dao/UserDao.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/model/room/dao/UserDao.kt
@@ -3,25 +3,17 @@ package com.seoultech.livingtogether_android.model.room.dao
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.room.*
+import com.seoultech.livingtogether_android.base.BaseDao
 import com.seoultech.livingtogether_android.model.room.entity.UserEntity
 
 @Dao
-interface UserDao {
+abstract class UserDao : BaseDao<UserEntity> {
     @Query("SELECT * FROM user_entity")
-    fun getAllObservable(): LiveData<UserEntity>
+    abstract fun getAllObservable(): LiveData<UserEntity>
 
     @Query("SELECT * FROM user_entity")
-    fun getAll(): UserEntity
+    abstract fun getAll(): UserEntity
 
-    @Insert
-    fun insert(userEntity: UserEntity)
-
-    @Update
-    fun update(userEntity: UserEntity)
-
-    @Delete
-    fun delete(userEntity: UserEntity)
-  
     @Query("DELETE FROM user_entity")
-    fun deleteAll()
+    abstract fun deleteAll()
 }
