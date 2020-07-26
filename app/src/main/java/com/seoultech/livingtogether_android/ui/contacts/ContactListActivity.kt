5bf895c5ae05b8c5d80ac84cf5c978012d7da977
김성규 <link5658@gmail.com>
2020-07-26 15:22:34 +0900

    update: layout of ContactListActivity

--- app/src/main/java/com/seoultech/livingtogether_android/ui/contacts/ContactListActivity.kt
+++ app/src/main/java/com/seoultech/livingtogether_android/ui/contacts/ContactListActivity.kt
@@ -1,6 +1,9 @@
 package com.seoultech.livingtogether_android.ui.contacts
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.seoultech.livingtogether_android.R
@@ -25,5 +28,39 @@ class ContactListActivity : BaseActivity<ActivityContactListBinding>(R.layout.ac
             recyclerContactContaclist.layoutManager = LinearLayoutManager(this@ContactListActivity, RecyclerView.VERTICAL, false)
             recyclerContactContaclist.adapter = contactListAdapter
         }
+
+        setSupportActionBar(binding.toolbar)
+
+        supportActionBar?.let {
+            it.setDisplayShowTitleEnabled(false)
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_keyboard_arrow_left_black_32dp)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_contact_list, menu)       // main_menu 메뉴를 toolbar 메뉴 버튼으로 설정
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+
+        when (item.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+
+            R.id.menu_go_to_add_activity -> {
+                //startActivity(Intent(this@ContactListActivity, SearchContactActivity::class.java))
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
